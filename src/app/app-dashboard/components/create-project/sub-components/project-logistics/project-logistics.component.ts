09dd8b594cd68a708d44d7ae87264e5c9582701c
EkhlasMridha <41969194+EkhlasMridha@users.noreply.github.com>
2020-12-27 01:23:12 +0600

    project evaluate

--- src/app/app-dashboard/components/create-project/sub-components/project-logistics/project-logistics.component.ts
+++ src/app/app-dashboard/components/create-project/sub-components/project-logistics/project-logistics.component.ts
@@ -8,20 +8,35 @@ import { title } from 'process';
   styleUrls: ['./project-logistics.component.scss']
 })
 export class ProjectLogisticsComponent implements OnInit {
-  manageProject: Partial<ServiceTypeModel>[] = [
+  // manageProject: Partial<ServiceTypeModel>[] = [
+  //   {
+  //     matIcon: "person_pin",
+  //     title: "Self Managed",
+  //   },
+  //   {
+  //     matIcon: "supervised_user_circle",
+  //     title: "Semi Managed",
+  //     subTitle: "We help you post the project, find a suitable vendor"
+  //   },
+  //   {
+  //     matIcon: "trip_origin",
+  //     title: "Fully Managed",
+  //     subTitle: "We help get your project executed end to end"
+  //   }
+  // ];
+
+  targetDateList: Partial<ServiceTypeModel>[] = [
     {
-      matIcon: "person_pin",
-      title: "Self Managed",
+      matIcon: "event_available",
+      title: "In next 2 weeks"
     },
     {
-      matIcon: "supervised_user_circle",
-      title: "Semi Managed",
-      subTitle: "We help you post the project, find a suitable vendor"
+      matIcon: "calendar_today",
+      title: "In next month"
     },
     {
-      matIcon: "trip_origin",
-      title: "Fully Managed",
-      subTitle: "We help get your project executed end to end"
+      matIcon: "event",
+      title: "Specific date"
     }
   ];
   constructor () { }
