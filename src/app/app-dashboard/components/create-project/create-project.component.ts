743174f0c2a76e848f24506e2a788049d253eb56
EkhlasMridha <41969194+EkhlasMridha@users.noreply.github.com>
2020-12-31 16:02:29 +0600

    prject step control

--- src/app/app-dashboard/components/create-project/create-project.component.ts
+++ src/app/app-dashboard/components/create-project/create-project.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { StepModel } from '@contracts/step.model';
+import { ProjectSteps } from '../../configs/project-step.config';
 
 @Component({
   selector: 'app-create-project',
@@ -8,9 +10,39 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class CreateProjectComponent implements OnInit {
   isLinear = true;
-  clientFinal: number = 4;
-  constructor (private _formBuilder: FormBuilder) { }
+  stepNumber: number = 4;
+  userType: string;
+  stepList: StepModel[];
+  constructor (private route: ActivatedRoute) {
+    this.userType = localStorage.getItem("userType");
+  }
 
   ngOnInit(): void {
+    this.stepList = this.filterProjectStep(ProjectSteps);
+    this.stepNumber = this.stepHandler(this.route, this.stepList);
+  }
+
+  filterProjectStep(stepList: StepModel[]) {
+    let data = stepList.filter(step => {
+      if (step.userType == "shared" || step.userType == this.userType.toLocaleLowerCase()) {
+        return step;
+      }
+    });
+
+    return data;
+  }
+
+  stepHandler(route: ActivatedRoute, stepList: StepModel[]): number {
+    let param = parseInt(route.snapshot.queryParams.p_step);
+
+    if (param == -1) {
+      return stepList.length - 1;
+    } else if (param) {
+      if (param > stepList.length - 1) {
+        return stepList.length - 1;
+      }
+      return param;
+    }
+    return 0;
   }
 }
---
+++ src/app/app-dashboard/configs/project-step.config.ts
@@ -0,0 +1,44 @@
+import { StepModel } from "@contracts/step.model";
+import { EvaluationProcessComponent } from "../components/create-project/sub-components/evaluation-process/evaluation-process.component";
+import { ProjectLogisticsComponent } from "../components/create-project/sub-components/project-logistics/project-logistics.component";
+import { ProjectOverviewComponent } from "../components/create-project/sub-components/project-overview/project-overview.component";
+import { ReviewComponent } from "../components/create-project/sub-components/review/review.component";
+import { TeamMembersComponent } from "../components/create-project/sub-components/team-members/team-members.component";
+
+export const ProjectSteps: StepModel[] = [
+    {
+        headerMatIcon: "description",
+        headerTitle: "Project Overview",
+        headerDescript: "Describe the project and select a category",
+        userType: "client",
+        component: ProjectOverviewComponent
+    },
+    {
+        headerMatIcon: "language",
+        headerTitle: "Project Logistics",
+        headerDescript: ">Provide project timelines and administration details",
+        userType: "client",
+        component: ProjectLogisticsComponent
+    },
+    {
+        headerMatIcon: "ballot",
+        headerTitle: "Evaluation Process",
+        headerDescript: "Describe additional project requirement",
+        userType: "client",
+        component: EvaluationProcessComponent
+    },
+    {
+        headerMatIcon: "people_outline",
+        headerTitle: "Team Members",
+        headerDescript: "Add your team members for seamless execution",
+        userType: "client",
+        component: TeamMembersComponent
+    },
+    {
+        headerMatIcon: "rate_review",
+        headerTitle: "Review",
+        headerDescript: "Review the project details before submitting",
+        userType: "client",
+        component: ReviewComponent
+    },
+];
